f1037716e7d5ee47d32306d2c4b32fb80d47c743
SinaMostafanejad <m_mostafanejad@hotmail.com>
2026-03-17 21:03:03 -0400

    updates the sft plotter script

--- scripts/plot_scripts/finetuning/perf_plotter.py
+++ scripts/plot_scripts/finetuning/perf_plotter.py
@@ -2,8 +2,9 @@
 # Author: Mohammad Mostafanejad                                               #
 # Date: February 2026                                                         #
 # Description:                                                                #
-# This module will use the 'ds_effect.xls' raw data file to plot the          #
-# dataset size effect on model performance.                                   #
+# This module will use the 'sft_adme_datasize_effect.xls' raw data file       #
+# to plot the dataset and model size effects on models' validation and        #
+# testing performance for downstream fine-tuning tasks.                       #
 ###############################################################################
 
 # import the necessary modules
@@ -49,7 +50,9 @@ def main(properties, mode: str = None):
         markers = {"tiny": "o", "small": "s", "base": "^"}
 
         # create fig and axes objects
-        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(6, 5), dpi=150, sharex=True, sharey=False)
+        fig, axes = plt.subplots(
+            nrows=2, ncols=2, figsize=(6, 5), dpi=150, sharex=True, sharey=False
+        )
 
         # create a list of y axis labels
         labels = ["Pearson $R$", "$R^2$", "RMSE", "MAE"]
@@ -74,8 +77,14 @@ def main(properties, mode: str = None):
                     errorbar=("ci", 95),
                     err_kws={"capsize": 3, "capthick": 1, "elinewidth": 1},
                 )
-                axes[row, col].set_title(plt_titles[i], loc="left", pad=10, fontweight="bold")
+                axes[row, col].set_title(
+                    plt_titles[i], loc="left", pad=10, fontweight="bold"
+                )
                 axes[row, col].set_ylabel(labels[i])
+                # make sure the y axis numbers are formatted with 2 decimal places
+                axes[row, col].yaxis.set_major_formatter(
+                    plt.FuncFormatter(lambda x, _: f"{x:.2f}")
+                )
         else:
             # bar plots for the test mode without error bars
             for i, metric in enumerate(metrics):
@@ -89,33 +98,57 @@ def main(properties, mode: str = None):
                     ax=axes[row, col],
                     errorbar=None,
                 )
-                axes[row, col].set_title(plt_titles[i], loc="left", pad=10, fontweight="bold")
+                axes[row, col].set_title(
+                    plt_titles[i], loc="left", pad=10, fontweight="bold"
+                )
                 axes[row, col].set_ylabel(labels[i])
+                # make sure the y axis numbers are formatted with 1 decimal place
+                axes[row, col].yaxis.set_major_formatter(
+                    plt.FuncFormatter(lambda x, _: f"{x:.1f}")
+                )
 
         for ax in axes.flatten():
             # set the x axis label to k
             ax.set_xlabel("$k$")
-            # make sure the y axis numbers are formatted with 1 decimal place
-            ax.yaxis.set_major_formatter(
-                plt.FuncFormatter(lambda x, _: f"{x:.1f}")
-            )
+
             if mode == "test":
                 # set the x axis ticks to 0, 1, 2 and label them as 0, 3, 5 for test mode
                 ax.set_xticks([0, 1, 2])
                 ax.set_xticklabels([0, 3, 5])
-                # set the y axis limits to 0, 0.7 for 
-                ax.set_ylim(0, 0.7)
-                
+                # set the y axis limits to 0, 0.8
+                ax.set_ylim(0, 0.8)
+                # remove y axis values from the second column
+                if ax in [axes[0, 1], axes[1, 1]]:
+                    ax.set_yticklabels([])
+                if ax in [axes[0, 0], axes[0, 1]]:
+                    ax2 = ax.secondary_xaxis("top")
+                    ax2.set_xticks([0, 1, 2])
+                    ax2.set_xticklabels(["2.5%", "20%", "80%"])
 
             # remove the lines connecting the markers in the line plots in val mode
             if mode == "val":
+                ax.set_xticks([0, 3, 5])
+                ax.set_xticklabels([0, 3, 5])
                 for line in ax.lines:
                     line.set_linestyle("")
-                    
+                # if marker = "^", set the marker size to 12
+                for line in ax.lines:
+                    if line.get_marker() == "^":
+                        line.set_markersize(8)
+                    else:
+                        line.set_markersize(6)
+                if ax in [axes[0, 0], axes[0, 1]]:
+                    ax2 = ax.secondary_xaxis("top")
+                    ax2.set_xticks([0, 3, 5])
+                    ax2.set_xticklabels(["2.5%", "20%", "80%"])
+
             # for the second graph, remove the legend title
-            ax.legend(title="")
-            # compact the legend
+            handles, labels = ax.get_legend_handles_labels()
+            labels = [label.capitalize() for label in labels]
             ax.legend(
+                handles,
+                labels,
+                title="",
                 loc="best",
                 frameon=True,
                 ncol=1,
@@ -124,6 +157,7 @@ def main(properties, mode: str = None):
                 columnspacing=0.3,
                 labelspacing=0.3,
             )
+
             # remove the legend for all but the second graph
             if ax != axes[0, 1]:
                 ax.legend().remove()
